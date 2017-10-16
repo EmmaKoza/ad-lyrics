6c9c8bf8835ecc4b050257d962956f4cc4dc3f08
EmmaKoza <emma.koza@gmail.com>
2017-10-15 20:45:45 -0400

    playing around with forms & values

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+
+const AdLib = (props) => {
+	return(
+		<div>
+    		I am a sentence with ${this.state.value} as a word
+    	</div>
+	)
+}
+
+class Form extends React.Component{
+	constructor(props){
+		super(props);	
+		this.handleChange = this.handleChange.bind(this);
+    	this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event){
+		this.setState({value: event.target.value});	
+	}
+	handleSubmit(event) {
+    	return(
+    		<AdLib/>		
+    	);
+  	}
+	render(){
+		return(
+			<div className="form-container">
+				<input type="text" id="noun"
+				value = {this.state.value}
+				onChange = {event => this.handleChange(event.target.value)} 
+				/>
+				<input type="submit" onSubmit ={event => this.handleSubmit(event.target.value)}/>
+				<div className="adLibs">
+					<AdLib words = {this.state.value}/>
+				</div>
+			</div>
+		);
+	}
+	
+}
+
+
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
+	render(){
+		
+		return(
+			<div>
+				<Form />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
